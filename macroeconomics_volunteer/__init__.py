27475de5e89249255dcf6c4900b619235d49c994
adaivr <adaivr@mail.ru>
2024-12-18 19:12:58 +0300

    Fixed macroeconomics volunteer

--- macroeconomics_volunteer/__init__.py
+++ macroeconomics_volunteer/__init__.py
@@ -1,24 +1,13 @@
 from otree.api import *
 
-c = cu
-
 doc = ""
 
-# Убрать фиксированное количество игроков
-# Подставить в формулу 
-# Выбор вложения труд или капитал
-# Страница результатов переписать
-# Страницу итогов за все раунды по всем игрокам
-# Сделать 10 периодов
-# Кнопку далее после результатов
 
 class C(BaseConstants):
     NAME_IN_URL = "macroeconomics_volunteer"
-    PLAYERS_PER_GROUP = 5
-    NUM_ROUNDS = 1
-    GENERAL_BENEFIT = cu(1)
-    NEEDED_PLAYERS = 3
-    TOTAL_PLAYERS = 5
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 10
+    LOG = {}
 
 
 class Subsession(BaseSubsession):
@@ -26,46 +15,103 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    num_volunteers = models.FloatField()
-    num_int = models.IntegerField()
+    chose_labor = models.IntegerField()
+    chose_capital = models.IntegerField()
+    total_players = models.IntegerField()
 
 
 def set_payoffs(group: Group):
     players = group.get_players()
-    group.num_volunteers = sum([p.volunteer == True for p in players])
-    group.num_int = sum([p.volunteer == True for p in players])
+    group.chose_labor = sum([p.chose_labor == True for p in players])
+    group.chose_capital = sum([p.chose_labor == False for p in players])
+    group.total_players = group.chose_labor + group.chose_capital
     for p in players:
-        if p.volunteer:
-            p.good = (
-                4.5 * ((group.num_volunteers - 1) * (C.TOTAL_PLAYERS - group.num_volunteers)
-                ) / ((C.TOTAL_PLAYERS - 2) * (C.TOTAL_PLAYERS - 1))
+        if p.chose_labor:
+            p.resources = (
+                4.5
+                * (group.chose_labor - 1)
+                * (group.chose_capital)
+                / (group.total_players - 1)
+                / (group.total_players - 2)
             )
         else:
-            p.good = 1
+            p.resources = 1
+        if p.id_in_group not in C.LOG:
+            C.LOG[p.id_in_group] = {"name": p.name, "chose_labor": [], "resources": []}
+        C.LOG[p.id_in_group]["resources"].append(p.resources)
+        C.LOG[p.id_in_group]["chose_labor"].append(p.chose_labor)
 
 
 class Player(BasePlayer):
-    volunteer = models.BooleanField(
-        doc="Whether player volunteers", label="Вложимся в труд?"
+    chose_labor = models.BooleanField(
+        label="Куда потратить ресурсы?",
+        choices=[
+            [False, "Капитал"],
+            [True, "Труд"],
+        ],
     )
-    good = models.FloatField()
+    name = models.StringField(label="Фамилия Имя:")
+    resources = models.FloatField()
+
+    def results(self):
+        results = []
+        for id in C.LOG:
+            name = C.LOG[id]["name"]
+            resources = sum(C.LOG[id]["resources"])
+            chose_labor = sum(C.LOG[id]["chose_labor"])
+            chose_capital = C.NUM_ROUNDS - chose_labor
+            results.append({
+                "name": name,
+                "resources": resources,
+                "chose_labor": chose_labor,
+                "chose_capital": chose_capital,
+            })
+        return results
+    def str_choice(self):
+        return "труд" if self.chose_labor else "капитал"
+
+
+class Registration(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == 1
+
+    form_model = "player"
+    form_fields = ["name"]
 
 
 class Rules(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == 1
+
+
+class Round(Page):
     form_model = "player"
+    form_fields = ["chose_labor"]
 
 
-class Decision(Page):
-    form_model = "player"
-    form_fields = ["volunteer"]
-
-
-class ResultsWaitPage(WaitPage):
+class RoundWaitPage(WaitPage):
     after_all_players_arrive = set_payoffs
 
 
+class RoundResults(Page):
+    pass
+
+
 class Results(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == C.NUM_ROUNDS
+
     form_model = "player"
 
 
-page_sequence = [Rules, Decision, ResultsWaitPage, Results]
+class End(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == C.NUM_ROUNDS
+    pass
+
+
+page_sequence = [Registration, Rules, Round, RoundWaitPage, RoundResults, Results, End]
